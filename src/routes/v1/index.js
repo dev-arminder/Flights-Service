925f55d3c9b3b5e79f7cd2b2fa2956b81bb47d75
Arminder Singh <arminder.singh@paxcom.net>
2023-09-04 00:45:31 +0530

    Setting Up Basic API And Project Structure

---
+++ src/routes/v1/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const { InfoController } = require('../../controllers');
+const airplaneRoutes = require('./airplane-routes');
+
+
+
+router.use('/airplanes', airplaneRoutes);
+router.get('/info', InfoController.info);
+
+
+module.exports = router;
