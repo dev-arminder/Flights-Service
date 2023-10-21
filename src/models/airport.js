2a18ea48502574f082ae45608d720d8907ad5693
dev-arminder <mail.to.arminder.singh@gmail.com>
2023-10-22 00:41:40 +0530

    Fixed Foreign key contraints in Airport / City Table

--- src/models/airport.js
+++ src/models/airport.js
@@ -11,9 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       this.belongsTo(models.City, {
-        foreignKey: 'cityId',
-        onDelete: 'CASCADE',
-        onUpdate: 'CASCADE'
+        foreignKey: 'cityId'
       });
     }
   }
