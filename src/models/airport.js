3731f21cae7c4f8d87dc57bb126e3e1e01552aca
dev-arminder <mail.to.arminder.singh@gmail.com>
2023-10-28 23:08:58 +0530

    Added flights model and setup fkey contraints

--- src/models/airport.js
+++ src/models/airport.js
@@ -13,6 +13,15 @@ module.exports = (sequelize, DataTypes) => {
       this.belongsTo(models.City, {
         foreignKey: 'cityId'
       });
+
+      this.hasMany(models.Flight, {
+        foreignKey: 'departureAirportId',
+        onDelete: 'CASCADE'
+      });
+      this.hasMany(models.Flight, {
+        foreignKey: 'arrivalAirportId',
+        onDelete: 'CASCADE'
+      });
     }
   }
   Airport.init({
